162d873d82d6e908bf266b8014f5ce5a81f513d0
Nikolay_Lyalkin <serega94nn@yandex.ru>
2024-10-12 12:42:08 +0300

    project init

--- src/web_start.py
+++ src/web_start.py
@@ -1,25 +1,36 @@
+# Импорт встроенной библиотеки для работы веб-сервера
 from http.server import BaseHTTPRequestHandler, HTTPServer
-import time
 
-hostName = "localhost"
-serverPort = 8080
+# Для начала определим настройки запуска
+hostName = "localhost"  # Адрес для доступа по сети
+serverPort = 8080  # Порт для доступа по сети
+
 
 class MyServer(BaseHTTPRequestHandler):
-    """Специальный класс, который отвечает за
-        обработку входящих запросов от клиентов"""
+    """
+    Специальный класс, который отвечает за
+    обработку входящих запросов от клиентов
+    """
 
     def do_GET(self):
+        """Метод для обработки входящих GET-запросов"""
+
+        path = "../html/contact.html"
+
         self.send_response(200)  # Отправка кода ответа
-        self.send_header("Content-type", "application/json")  # Отправка типа данных, который будет передаваться
+        self.send_header("Content-type", "text/html")  # Отправка типа данных, который будет передаваться
         self.end_headers()  # Завершение формирования заголовков ответа
-        self.wfile.write(bytes("{'message': 'OK'}", "utf-8"))  # Тело ответа
+        with open(path, encoding="utf-8") as file:
+            content = file.read()
+            self.wfile.write(bytes(content, "utf-8"))  # Тело ответа
 
     def do_POST(self):
-        content_length = int(self.headers['Content-Length'])
-        body = self.rfile.read(content_length)
-        print(body)
-        self.send_response(200)
-        self.end_headers()
+        content_length = int(self.headers["Content-Length"])
+        post_data = self.rfile.read(content_length)
+
+        response = f"Received POST data: {post_data.decode('utf-8')}"
+        print(response)
+
 
 if __name__ == "__main__":
     # Инициализация веб-сервера, который будет по заданным параметрах в сети
