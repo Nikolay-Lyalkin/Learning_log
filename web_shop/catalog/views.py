caed0df06409a53389fe6734ab926447e1e88767
Nikolay_Lyalkin <serega94nn@yandex.ru>
2024-10-23 14:07:02 +0300

    homework

--- web_shop/catalog/views.py
+++ web_shop/catalog/views.py
@@ -1,41 +1,67 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.views.generic import DetailView, ListView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.core.paginator import Paginator
 
 from .models import Contact, Product, Category
 
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = "catalog/product.html"
+    context_object_name = "product"
+
+
+class ProductCreateView(CreateView):
+    model = Product
+    fields = ["name", "description", "image", "category", "price"]
+    template_name = "catalog/form_add_product.html"
+    context_object_name = "product"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['categories'] = Category.objects.all()
+        return context
+
+
+class ProductListView(ListView):
+    model = Product
+    template_name = "catalog/home.html"
+    context_object_name = "products"
+    paginate_by = 3
+
 
 # Create your views here.
-def home_views(request):
-    if request.method == "POST":
-        name = request.POST.get("name")
-        description = request.POST.get("description")
-        image = request.POST.get("image")
-        categories = Category.objects.all()
-        category = [category for category in categories if request.POST.get("category") == category.name]
-        price = request.POST.get("price")
-
-        new_product = Product.objects.create(
-            name=name, description=description, image=image, category=category[0], price=price
-        )
-
-        # Здесь мы просто возвращаем простой ответ
-        return HttpResponse(f"Вы добавили товар - {name}!")
-
-    categories = Category.objects.all()
-    products = Product.objects.all()
-
-    paginator = Paginator(products, 3)
-    page_number = request.GET.get("page", 1)
-    page_obj = paginator.get_page(page_number)
-
-    context = {
-        "products": products,
-        "categories": categories,
-        "page_obj": page_obj,
-    }
-
-    return render(request, "catalog/home.html", context)
+# def home_views(request):
+#     if request.method == "POST":
+#         name = request.POST.get("name")
+#         description = request.POST.get("description")
+#         image = request.POST.get("image")
+#         categories = Category.objects.all()
+#         category = [category for category in categories if request.POST.get("category") == category.name]
+#         price = request.POST.get("price")
+#
+#         new_product = Product.objects.create(
+#             name=name, description=description, image=image, category=category[0], price=price
+#         )
+#
+#         # Здесь мы просто возвращаем простой ответ
+#         return HttpResponse(f"Вы добавили товар - {name}!")
+#
+#     categories = Category.objects.all()
+#     products = Product.objects.all()
+#
+#     paginator = Paginator(products, 3)
+#     page_number = request.GET.get("page", 1)
+#     page_obj = paginator.get_page(page_number)
+#
+#     context = {
+#         "products": products,
+#         "categories": categories,
+#         "page_obj": page_obj,
+#     }
+#
+#     return render(request, "catalog/home.html", context)
 
 
 def category_views(request):
@@ -59,7 +85,7 @@ def contact_views(request):
     return render(request, "catalog/contact.html", contact)
 
 
-def product_views(request, product_id):
-    product = Product.objects.get(id=product_id)
-    context = {"product": product}
-    return render(request, "catalog/product.html", context)
+# def product_views(request, product_id):
+#     product = Product.objects.get(id=product_id)
+#     context = {"product": product}
+#     return render(request, "catalog/product.html", context)
